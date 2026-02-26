876194ccd114af914117eb5210d1f02918f971bb
YvaishaliR <YvaishaliR>
2026-02-26 14:29:17 +0530

    Add main.connectivity.hub.and.spoke.virtual.network.tf [skip ci]

---
+++ main.connectivity.hub.and.spoke.virtual.network.tf
@@ -0,0 +1,16 @@
+module "hub_and_spoke_vnet" {
+  source  = "Azure/avm-ptn-alz-connectivity-hub-and-spoke-vnet/azurerm"
+  version = "0.16.11"
+
+  count = local.connectivity_hub_and_spoke_vnet_enabled ? 1 : 0
+
+  hub_and_spoke_networks_settings = local.hub_and_spoke_networks_settings
+  hub_virtual_networks            = local.hub_virtual_networks
+  enable_telemetry                = var.enable_telemetry
+  tags                            = coalesce(module.config.outputs.connectivity_tags, module.config.outputs.tags)
+
+  providers = {
+    azurerm = azurerm.connectivity
+    azapi   = azapi.connectivity
+  }
+}
