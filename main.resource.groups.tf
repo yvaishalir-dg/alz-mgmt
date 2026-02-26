2383ab9060ecce9a7dcc234de253a20900ce6d47
YvaishaliR <YvaishaliR>
2026-02-26 14:28:17 +0530

    Add main.resource.groups.tf [skip ci]

---
+++ main.resource.groups.tf
@@ -0,0 +1,15 @@
+module "resource_groups" {
+  source  = "Azure/avm-res-resources-resourcegroup/azurerm"
+  version = "0.2.1"
+
+  for_each = { for key, value in module.config.outputs.connectivity_resource_groups : key => value if try(value.settings.enabled, true) }
+
+  name             = each.value.name
+  location         = each.value.location
+  enable_telemetry = var.enable_telemetry
+  tags             = try(each.value.tags, null) == null ? module.config.outputs.tags : each.value.tags
+
+  providers = {
+    azurerm = azurerm.connectivity
+  }
+}
