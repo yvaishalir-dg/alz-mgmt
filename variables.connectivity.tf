3d008792840b3e2bfdb0b0b50e98322d7e018db2
YvaishaliR <YvaishaliR>
2026-02-26 14:28:54 +0530

    Add variables.connectivity.tf [skip ci]

---
+++ variables.connectivity.tf
@@ -0,0 +1,35 @@
+variable "connectivity_type" {
+  type        = string
+  description = "The type of network connectivity technology to use for the private DNS zones"
+  default     = "hub_and_spoke_vnet"
+  validation {
+    condition     = contains(values(local.const.connectivity), var.connectivity_type)
+    error_message = "The connectivity type must be either 'hub_and_spoke_vnet', 'virtual_wan' or 'none'"
+  }
+}
+
+variable "connectivity_resource_groups" {
+  type = map(object({
+    name     = string
+    location = string
+    tags     = optional(map(string))
+    settings = optional(any)
+  }))
+  default     = {}
+  description = <<DESCRIPTION
+A map of resource groups to create. These must be created before the connectivity module is applied.
+
+The following attributes are supported:
+
+  - name: The name of the resource group
+  - location: The location of the resource group
+  - settings: (Optional) An object, which can include an `enabled` setting value that indicates whether the resource group should be created.
+
+DESCRIPTION
+}
+
+variable "connectivity_tags" {
+  type        = map(string)
+  default     = null
+  description = "(Optional) Tags of the connectivity resources."
+}
