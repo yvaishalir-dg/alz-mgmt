48dac5aae0661b4114032b7c935efe569fb18ba9
YvaishaliR <YvaishaliR>
2026-02-26 14:29:52 +0530

    Add terraform.tf [skip ci]

---
+++ terraform.tf
@@ -0,0 +1,73 @@
+terraform {
+  required_version = "~> 1.12"
+  required_providers {
+    alz = {
+      source  = "Azure/alz"
+      version = "0.20.2"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5"
+    }
+  }
+  backend "azurerm" {}
+}
+
+provider "alz" {
+  library_overwrite_enabled = true
+  library_references = [
+    {
+      custom_url = "${path.root}/lib"
+    }
+  ]
+}
+
+provider "azapi" {
+  skip_provider_registration = true
+  subscription_id            = try(var.subscription_ids["management"], var.subscription_id_management)
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  alias                           = "management"
+  subscription_id                 = try(var.subscription_ids["management"], var.subscription_id_management)
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azurerm" {
+  resource_provider_registrations = "none"
+  alias                           = "connectivity"
+  subscription_id                 = try(var.subscription_ids["connectivity"], var.subscription_id_connectivity)
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+provider "azapi" {
+  alias                      = "connectivity"
+  skip_provider_registration = true
+  subscription_id            = try(var.subscription_ids["connectivity"], var.subscription_id_connectivity)
+}
