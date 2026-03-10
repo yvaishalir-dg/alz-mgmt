f53c99d6b74d25ef4582594988ee075e3c8ffb80
YvaishaliR <YvaishaliR>
2026-03-10 14:29:11 +0530

    Add main.connectivity.virtual.wan.tf [skip ci]

---
+++ main.connectivity.virtual.wan.tf
@@ -0,0 +1,15 @@
+module "virtual_wan" {
+  source  = "Azure/avm-ptn-alz-connectivity-virtual-wan/azurerm"
+  version = "0.13.5"
+
+  count = local.connectivity_virtual_wan_enabled ? 1 : 0
+
+  virtual_wan_settings = local.virtual_wan_settings
+  virtual_hubs         = local.virtual_hubs
+  enable_telemetry     = var.enable_telemetry
+  tags                 = coalesce(module.config.outputs.connectivity_tags, module.config.outputs.tags)
+
+  providers = {
+    azurerm = azurerm.connectivity
+  }
+}
